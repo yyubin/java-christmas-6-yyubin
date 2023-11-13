170c04400ead67c791914d224b8922eb2ce28d6e
yyubin <hazing120@gmail.com>
2023-11-14 06:04:14 +0900

    fix: 날짜 예외 조건 추가

--- src/main/java/christmas/util/DateValidator.java
+++ src/main/java/christmas/util/DateValidator.java
@@ -7,7 +7,7 @@ public class DateValidator {
     public static boolean validateDate(String orderDate) {
         try {
             int parsedDate = Integer.parseInt(orderDate);
-            if (parsedDate <= 0) {
+            if (parsedDate <= 0 || parsedDate > 32) {
                 OutputView.print(Messages.INVALID_DATE_ERROR);
                 return false;
             }
