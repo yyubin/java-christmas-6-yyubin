5cf64ad726be8ac102a070a61cf0ce14b686f522
yyubin <hazing120@gmail.com>
2023-11-14 06:17:55 +0900

    fix: 날짜 범위 변경

--- src/main/java/christmas/validator/DateValidator.java
+++ src/main/java/christmas/validator/DateValidator.java
@@ -6,7 +6,7 @@ public class DateValidator {
     public static int validateDate(String orderDate) {
         try {
             int parsedDate = Integer.parseInt(orderDate);
-            if (parsedDate <= 0 || parsedDate > 32) {
+            if (parsedDate <= 0 || parsedDate > 31) {
                 throw new IllegalArgumentException(Messages.INVALID_DATE_ERROR);
             }
             return parsedDate;
