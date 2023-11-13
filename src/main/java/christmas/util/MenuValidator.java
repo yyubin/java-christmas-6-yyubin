14b9b7eab5d0e1f92e575053da848363061cce11
yyubin <hazing120@gmail.com>
2023-11-14 01:46:57 +0900

    feat: 메뉴 유효성 검사 기능 추가

--- src/main/java/christmas/util/MenuValidator.java
+++ src/main/java/christmas/util/MenuValidator.java
@@ -1,6 +1,8 @@
 package christmas.util;
 
 import christmas.model.Menu;
+import christmas.model.MenuType;
+import christmas.model.OrderMenu;
 import christmas.view.Messages;
 
 import java.util.Arrays;
@@ -38,6 +40,30 @@ public class MenuValidator {
 
     private static void validatePositiveQuantity(int quantity) {
         if (quantity <= 0) {
+            throw new IllegalArgumentException(Messages.INVALID_ORDER_ERROR);
+        }
+    }
+
+    public static void validateNonExclusiveBeverageOrder(List<OrderMenu> orderMenus) {
+        boolean hasNonBeverage = false;
+
+        for (OrderMenu order : orderMenus) {
+            if (order.getMenu().getType() != MenuType.BEVERAGE && order.getQuantity() > 0) {
+                hasNonBeverage = true;
+            }
+        }
+
+        if (!hasNonBeverage) {
+            validateFailOnlyBeverageOrder(false);
+        }
+    }
+
+    private static void validateFailOnlyBeverageOrder(boolean isOnlyBeverage) {
+        throw new IllegalArgumentException(Messages.INVALID_ORDER_ERROR);
+    }
+
+    public static void validateMaxOrderCount(List<OrderMenu> orderMenus) {
+        if (orderMenus.size() > 20) {
             throw new IllegalArgumentException(Messages.INVALID_ORDER_ERROR);
         }
     }
