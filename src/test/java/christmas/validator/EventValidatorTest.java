b2778b16fbc2a6efaa6752638602d8496145e09b
yyubin <hazing120@gmail.com>
2023-11-15 03:03:40 +0900

    test: 이벤트 유효성 검사 기능 테스트

--- src/test/java/christmas/validator/EventValidatorTest.java
+++ src/test/java/christmas/validator/EventValidatorTest.java
@@ -1,4 +1,20 @@
 package christmas.validator;
 
+import christmas.validator.EventValidator;
+import christmas.config.TotalOrderAmount;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class EventValidatorTest {
+
+    @Test
+    public void testValidateTotalOrderAmount() {
+        EventValidator eventValidator = new EventValidator();
+
+        assertFalse(eventValidator.validateTotalOrderAmount(TotalOrderAmount.EVENT_GIFT_THRESHOLD.getAmount() - 1));
+
+        assertTrue(eventValidator.validateTotalOrderAmount(TotalOrderAmount.EVENT_GIFT_THRESHOLD.getAmount()));
+
+        assertTrue(eventValidator.validateTotalOrderAmount(TotalOrderAmount.EVENT_GIFT_THRESHOLD.getAmount() + 1));
+    }
 }
